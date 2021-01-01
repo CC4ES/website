cddf7510e335f50d86d7967345f6bc8f39bff5cb
Jason Gorelick <821247+jasongorelick@users.noreply.github.com>
2021-01-01 14:30:47 -0800

    removes unused files

--- Rakefile
+++
@@ -1,6 +0,0 @@
-require 'html-proofer'
-
-task :test do
-  sh "bundle exec middleman build"
-  HTMLProofer.check_directory("./build").run
-end
--- helpers/markdown_helper.rb
+++
@@ -1,7 +0,0 @@
-module MarkdownHelper
-  def inline_markdown(text)
-    # Redcarpet definitely exists because I specified it in config.rb and it's in the Gemfile
-    rc = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
-    rc.render(text).html_safe
-  end
-end
