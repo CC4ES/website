d382d59bc0ecd0c0736005e240ab59b3c0b05ba2
Sam Esfahani <ssesfahani@gmail.com>
2022-10-03 19:06:36 -0400

    Fix Hero stylings

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    backgroundPosition: {
+      hero: "0 100%",
+    },
     container: {
       center: true,
       padding: {
@@ -15,7 +18,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         "hero-pattern":
-          "linear-gradient(to right bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/seedlings_bg.jpg')",
+          "linear-gradient(to right bottom, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url('/seedlings_bg_large.jpg')",
         "footer-pattern":
           "linear-gradient(to right bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/aloe_leaves_bg.jpg')",
         "heading-pattern":
