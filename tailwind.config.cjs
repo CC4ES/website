cd8bf5a43b6e5d577a63f1571cdc51cc89b6d382
Sam Esfahani <ssesfahani@gmail.com>
2022-10-03 21:11:10 -0400

    Adjust screens responsive system

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,4 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -7,23 +8,33 @@ module.exports = {
     },
     container: {
       center: true,
-      padding: {
-        DEFAULT: "5rem",
-        sm: "5rem",
-        lg: "5rem",
-        xl: "5rem",
-        "2xl": "5rem",
-      },
+      padding: "1rem",
     },
     extend: {
       backgroundImage: {
         "hero-pattern":
           "linear-gradient(to right bottom, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url('/seedlings_bg_large.jpg')",
         "footer-pattern":
-          "linear-gradient(to right bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/aloe_leaves_bg.jpg')",
+          "linear-gradient(to right bottom, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('/aloe_leaves_bg.jpg')",
         "heading-pattern":
           "linear-gradient(to right bottom, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url('/aloe_leaves_bg.jpg')",
       },
+    },
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "75em",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "75em",
+      // => @media (min-width: 1536px) { ... }
     },
   },
   plugins: [],
