78e9d8deba6f375d6bf256a27fac92ce806aaa93
Sam Esfahani <ssesfahani@gmail.com>
2022-10-04 22:48:33 -0400

    Make mobile hamburger icon bigger

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -157,7 +157,7 @@ export function NavBarDesktop() {
         <div className="-my-2 -mr-2 md:hidden">
           <Popover.Button className="inline-flex items-center justify-center rounded-md bg-lime-100 p-2 text-gray-800 hover:bg-lime-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-700">
             <span className="sr-only">Open menu</span>
-            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            <Bars3Icon className="h-9 w-9" aria-hidden="true" />
           </Popover.Button>
         </div>
         <Popover.Group as="nav" className="hidden space-x-10 md:flex">
