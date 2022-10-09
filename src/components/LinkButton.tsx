2ce0ba65a59b460098a413cb9b1507402aa60aaf
Sam Esfahani <ssesfahani@gmail.com>
2022-10-09 14:58:48 -0400

    Match LinkButton with Links

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -9,7 +9,7 @@ export default function LinkButton(props: Props) {
     <a
       href={href}
       onClick={onClick}
-      className="inline-flex justify-center cursor-pointer py-2 px-3 text-lg font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300"
+      className="inline-flex justify-center cursor-pointer py-2 px-3 text-lg font-medium text-center text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300"
     >
       {children}
     </a>
